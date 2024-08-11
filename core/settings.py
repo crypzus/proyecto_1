cfa45d6c119875596a705ae98b3b44fd7730b016
crypzus <mejiasernestor@gmail.com>
2024-08-11 13:47:51 -0500

    defniendo el motor de base de datos y otras configuraciones faltantes del servidor djando

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import os
+import environ
+
+#se utliza cuando queremos haver variables de ambiente
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,17 +14,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$0m*g64lepx(8u*m1(+m8#34jrkf*^cbswg!sa!7m8x7-%%o^5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -26,8 +32,38 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+#icluye la aplicaciones que creamos como lo usarios, los blods 
+PROJET_APPS = [
+    
+]
+#inculye  los paquete de requerimientos. txt
+THIRD_PARTY_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'ckeditor',
+    'ckeditor_uploader'
+]
+#luego de definir PROJET_APPS, THIRD_PARTY_APPS y DJANGO_APPS craamos installed apps
+INSTALLED_APPS = DJANGO_APPS + PROJET_APPS + THIRD_PARTY_APPS
+
+#ckeditor
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source']
+        ],
+        'autoParagraph': False
+    },
+}
+#para guardar imagenes videos que se suban al sitio
+CKEDITOR_UPLOAD_PATH = "/media/"
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -91,9 +127,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC-5'
 
 USE_I18N = True
 
@@ -105,9 +141,48 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+#esta carpeta se a crear cuando se ejecute una funcion que se llama colectstatic
+# son los archivos que contienen nuestro ccs, entre otros
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+#esta carpeta va ser donde se guarde todo el multimedia de la pagina
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+#esto es para el proyecto react
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        #la gestion de permisos AllowAny, IsAuthenticatedOrReadOnly o IsAuthenticated 
+        # hasta donde quiere que llegue el usario para editar en el sitio web
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
+#se utilizan para configurar la lista de dominios que están permitidos a hacer solicitudes CORS 
+# (Cross-Origin Resource Sharing) a tu servidor Django.
+CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST_DEV')
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEV')
+
+#configuracion de email que se utliliza en modo desarrolador 
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+
+if not DEBUG:
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEPLOY')
+    CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST_DEPLOY')
+    CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEPLOY')
+    
+    DATABASES = {
+        "default": env.db("DATABASE_URL"),
+        
+    }
+    DATABASES["default"]["ATOMIC_REQUESTS"] = True
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+#se importa para ara configurar la gestión de archivos estáticos y medios 
+#en Django durante el desarrollo.
+from django.conf.urls.static  import static
+#se importa para vincular el arhivo settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
